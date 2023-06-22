34df1736222e39945cdf7c20dba2b9cdf0842ee1
Bbondchen <128323709+Bbondchen@users.noreply.github.com>
2023-06-22 15:26:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function onClick(event) {
+  document.getElementById("changeRed").style.color = "black";
+  document.getElementById("changeOrange").style.color = "black";
+  document.getElementById("changeYellow").style.color = "black";
+  //document..style.color
+  //alert("i got klicked")
+});
+
+/*
+ document.querySelector('rot').
+ document.querySelector('orange').style.color = 'black';
+ document.querySelector('gelb').style.color = 'black';
+
+}*/
+/*id=changeRed
+changeOrange
+changeYellow*/
